004c33010788ff183ae1c15b54c25704528c3a5b
andrew-saydjari <saydjari@g.harvard.edu>
2023-12-08 23:29:03 -0700

    day 08

--- src/day01.jl
+++ src/day01.jl
@@ -29,6 +29,7 @@ solution(input)
 name2digit = Dict("one"=>"1","two"=>"2","three"=>"3","four"=>"4","five"=>"5","six"=>"6","seven"=>"7","eight"=>"8","nine"=>"9",
     "1"=>"1","2"=>"2","3"=>"3","4"=>"4","5"=>"5","6"=>"6","7"=>"7","8"=>"8","9"=>"9")
 
+    
 reg = @compile either(DIGIT,"one","two","three","four","five","six","seven","eight","nine")
 regex = r"(?:(?:\d)|(?:one)|(?:two)|(?:three)|(?:four)|(?:five)|(?:six)|(?:seven)|(?:eight)|(?:nine))"
 
